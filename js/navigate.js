b7931f27f9d38f7e4ce975ba674a3c8f79fd3c61
LINDA <lindamac@icecreamhabit.local>
2016-08-16 20:37:03 +0300

    Added smooth navigate

---
+++ js/navigate.js
@@ -0,0 +1,17 @@
+
+/*
+ * Funktio, jolla navigointi tapahtuu sulavasti.
+ */
+
+$(function() {
+	$('li a').bind('click', function(event) {
+
+		var $anchor = $(this);
+
+		$('html, body').stop().animate({scrollTop: $($anchor.attr('href')).offset().top}, 1500, 'easeInOutExpo');
+
+		event.preventDefault();
+	});
+
+
+});
